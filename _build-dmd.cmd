086c68b3028aa96ad74d73fb953ae51cec9b2225
Vladimir Panteleev <vladimir@thecybershadow.net>
2014-01-29 10:47:49 +0000

    _build-dmd.cmd: Build with DMC if VC build fails

--- _build-dmd.cmd
+++ _build-dmd.cmd
@@ -10,16 +10,28 @@ if not defined CONFIGURATION set CONFIGURATION=Release
 if not defined PLATFORM set PLATFORM=x64
 
 msbuild /p:Configuration=%CONFIGURATION% /p:Platform=%PLATFORM% dmd_msc_vs10.sln
-if errorlevel 1 exit 1
+if errorlevel 1 goto dmc
 
 copy /y vcbuild\%PLATFORM%\%CONFIGURATION%\dmd_msc.exe dmd.exe
 copy /y vcbuild\%PLATFORM%\%CONFIGURATION%\dmd_msc.pdb dmd.pdb
 C:\cygwin\bin\touch dmd.exe
 
-:: Run unit tests
+goto install
 
-dmd @
+:dmc
+:: MSVC build failed, try DMC
+set PATH=\dm\bin;%WINDIR%\System32;C:\Soft\Tools
 
+if exist dmd.exe del dmd.exe
+if exist dmd.pdb del dmd.pdb
+make -f win32.mak clean
+
+make -f win32.mak
+if errorlevel 1 exit 1
+cv2pdb -C dmd.exe
+if errorlevel 1 exit 1
+
+:install
 :: Install
 copy /y dmd.exe ..\..\..\out\windows\bin
 copy /y dmd.pdb ..\..\..\out\windows\bin
