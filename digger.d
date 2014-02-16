c4f82c556d91abee2e437fec448c8aa00f949342
Vladimir Panteleev <vladimir@thecybershadow.net>
2014-02-16 22:21:01 +0000

    digger: Add build command

--- digger.d
+++ digger.d
@@ -1,8 +1,10 @@
 module digger;
 
 import std.exception;
+import std.getopt;
 
 import bisect;
+import build;
 import common;
 import repo;
 
@@ -10,19 +12,34 @@ import ae.sys.windows; // http://d.puremagic.com/issues/show_bug.cgi?id=7016
 
 int main()
 {
-	enforce(opts.args.length, "No command specified");
-	switch (opts.args[0])
+	auto args = opts.args.dup;
+	enforce(args.length, "No command specified");
+	switch (args[0])
 	{
 		case "bisect":
 			return doBisect();
 		case "show":
 		{
-			enforce(opts.args.length == 2, "Specify revision");
-			auto rev = parseRev(opts.args[1]);
+			enforce(args.length == 2, "Specify revision");
+			auto rev = parseRev(args[1]);
 			Repository(repoDir).run("log", "-n1", rev);
 			return 0;
 		}
+		case "build":
+		{
+			bool model64;
+			getopt(args,
+				"--64", &model64,
+			);
+			enforce(args.length == 2, "Specify revision");
+			prepareRepo(true);
+			auto rev = parseRev(args[1]);
+			Repository(repoDir).run("checkout", rev);
+			prepareTools();
+			enforce(prepareBuild(), "Build failed");
+			return 0;
+		}
 		default:
-			throw new Exception("Unknown command: " ~ opts.args[0]);
+			throw new Exception("Unknown command: " ~ args[0]);
 	}
 }
