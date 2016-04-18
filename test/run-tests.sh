35ba35f64030a0cd44ace65fbd9ce6f0853d9d04
Vladimir Panteleev <git@thecybershadow.net>
2016-04-18 03:13:30 +0000

    test: Use make -j

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -3,6 +3,8 @@ set -euxo pipefail
 
 cd "$(dirname "$0")"
 
+CPUCOUNT="$(getconf _NPROCESSORS_ONLN)"
+
 echo "workDir = $(pwd)/work/" > ./digger.ini
 echo "cache = git" >> ./digger.ini
 
@@ -18,30 +20,33 @@ rdmd --build-only -cov -g -of./digger ../digger.d
 
 # Simple build
 
-./digger --config-file ./digger.ini build "master @ 2016-01-01 00:00:00"
+./digger --config-file ./digger.ini build --make-args=-j"$CPUCOUNT" "master @ 2016-01-01 00:00:00"
 work/result/bin/dmd -run issue15914.d
 
 # Caching
 
-./digger --config-file ./digger.ini --offline build "master @ 2016-01-01 00:00:00" 2>&1 | tee digger.log
+./digger --config-file ./digger.ini --offline build --make-args=-j"$CPUCOUNT" "master @ 2016-01-01 00:00:00" 2>&1 | tee digger.log
 ! grep --quiet --fixed-strings --line-regexp 'digger: Cache miss.' digger.log
 grep --quiet --fixed-strings --line-regexp 'digger: Cache hit!' digger.log
 
 # Merging
 
-./digger --config-file ./digger.ini --offline build "master @ 2016-01-01 00:00:00 + phobos#3859"
+./digger --config-file ./digger.ini build --make-args=-j"$CPUCOUNT" "master @ 2016-01-01 00:00:00 + phobos#3859"
 ! work/result/bin/dmd -run issue15914.d
 
 # Reverting
 
-./digger --config-file ./digger.ini --offline build "master @ 2016-04-01 00:00:00 + -phobos#3859"
+./digger --config-file ./digger.ini --offline build --make-args=-j"$CPUCOUNT" "master @ 2016-04-01 00:00:00 + -phobos#3859"
 work/result/bin/dmd -run issue15914.d
 
 # Bisecting
 
-echo 'bad  = master @ 2016-04-01' >  bisect.ini
-echo 'good = master @ 2016-01-01' >> bisect.ini
-echo 'tester = dmd -run issue15914.d' >> bisect.ini
+cat > bisect.ini <<EOF
+bad  = master @ 2016-04-01
+good = master @ 2016-01-01
+tester = dmd -run issue15914.d
+build.components.common.makeArgs = ["-j$CPUCOUNT"]
+EOF
 
 ./digger --config-file ./digger.ini --offline bisect ./bisect.ini 2>&1 | tee digger.log
 diff <(tail -n 19 digger.log) issue15914-bisect.log
