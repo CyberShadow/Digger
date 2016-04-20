de502d7fe06f3d4a2b136fd5f4a0300077225235
Vladimir Panteleev <git@thecybershadow.net>
2016-04-20 13:26:50 +0000

    test: Use --jobs instead of --make-args
    --make-args prevented running the test script on Windows.

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -3,8 +3,6 @@ set -euxo pipefail
 
 cd "$(dirname "$0")"
 
-CPUCOUNT="$(getconf _NPROCESSORS_ONLN)"
-
 echo "workDir = $(pwd)/work/" > ./digger.ini
 echo "cache = git" >> ./digger.ini
 
@@ -20,7 +18,7 @@ rdmd --build-only -cov -debug -g -of./digger ../digger.d
 
 # Simple build
 
-./digger --config-file ./digger.ini build --make-args=-j"$CPUCOUNT" "master @ 2016-01-01 00:00:00"
+./digger --config-file ./digger.ini build --jobs=auto "master @ 2016-01-01 00:00:00"
 work/result/bin/dmd -run issue15914.d
 
 # Run tests
@@ -35,17 +33,17 @@ if [ "$(uname)" == "Darwin" ]; then
 	# TODO, rdmd bug: https://travis-ci.org/CyberShadow/Digger/jobs/124429436
 	TEST_ARGS+=('--without=rdmd')
 fi
-./digger --config-file ./digger.ini test "${TEST_ARGS[@]}" --make-args=-j"$CPUCOUNT" # Without DMD as that takes too long and is too fragile
+./digger --config-file ./digger.ini test "${TEST_ARGS[@]}" --jobs=auto # Without DMD as that takes too long and is too fragile
 
 # Caching
 
-./digger --config-file ./digger.ini --offline build --make-args=-j"$CPUCOUNT" "master @ 2016-01-01 00:00:00" 2>&1 | tee digger.log
+./digger --config-file ./digger.ini --offline build --jobs=auto "master @ 2016-01-01 00:00:00" 2>&1 | tee digger.log
 ! grep --quiet --fixed-strings --line-regexp 'digger: Cache miss.' digger.log
 grep --quiet --fixed-strings --line-regexp 'digger: Cache hit!' digger.log
 
 # Rebuild - no changes
 
-./digger --config-file ./digger.ini rebuild --make-args=-j"$CPUCOUNT"
+./digger --config-file ./digger.ini rebuild --jobs=auto
 work/result/bin/dmd -run issue15914.d
 
 # Rebuild - with changes
@@ -54,31 +52,31 @@ pushd work/repo/phobos/
 git cherry-pick --no-commit ad226e92d5f092df233b90fd3fdedb8b71d728eb
 popd
 
-./digger --config-file ./digger.ini rebuild --make-args=-j"$CPUCOUNT"
+./digger --config-file ./digger.ini rebuild --jobs=auto
 ! work/result/bin/dmd -run issue15914.d
 
 # Working tree state
 
-! ./digger --config-file ./digger.ini build --make-args=-j"$CPUCOUNT" "master @ 2016-04-01 00:00:00" # Worktree is dirty - should fail
+! ./digger --config-file ./digger.ini build --jobs=auto "master @ 2016-04-01 00:00:00" # Worktree is dirty - should fail
 rm work/repo/.git/modules/phobos/ae-sys-d-worktree.json
-  ./digger --config-file ./digger.ini build --make-args=-j"$CPUCOUNT" "master @ 2016-04-01 00:00:00" # Should work now
+  ./digger --config-file ./digger.ini build --jobs=auto "master @ 2016-04-01 00:00:00" # Should work now
 ! work/result/bin/dmd -run issue15914.d
 
 # Merging
 
-./digger --config-file ./digger.ini build --make-args=-j"$CPUCOUNT" "master @ 2016-01-01 00:00:00 + phobos#3859"
+./digger --config-file ./digger.ini build --jobs=auto "master @ 2016-01-01 00:00:00 + phobos#3859"
 ! work/result/bin/dmd -run issue15914.d
 
 # Cached merging
 
-./digger --config-file ./digger.ini --offline build --make-args=-j"$CPUCOUNT" "master @ 2016-01-01 00:00:00 + phobos#3859" 2>&1 | tee digger.log
+./digger --config-file ./digger.ini --offline build --jobs=auto "master @ 2016-01-01 00:00:00 + phobos#3859" 2>&1 | tee digger.log
 ! grep --quiet --fixed-strings --line-regexp 'digger: Cache miss.' digger.log
 ! grep --quiet --fixed-strings --line-regexp 'digger: Merging phobos commit ad226e92d5f092df233b90fd3fdedb8b71d728eb' digger.log
 grep --quiet --fixed-strings --line-regexp 'digger: Cache hit!' digger.log
 
 # Reverting
 
-./digger --config-file ./digger.ini --offline build --make-args=-j"$CPUCOUNT" "master @ 2016-04-01 00:00:00 + -phobos#3859"
+./digger --config-file ./digger.ini --offline build --jobs=auto "master @ 2016-04-01 00:00:00 + -phobos#3859"
 work/result/bin/dmd -run issue15914.d
 
 # Bisecting
