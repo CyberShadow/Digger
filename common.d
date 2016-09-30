64df671aa5ecd2a552d25a2503c1602b97f3504e
Vladimir Panteleev <git@thecybershadow.net>
2016-09-30 19:05:29 +0000

    common: Update version

--- common.d
+++ common.d
@@ -2,7 +2,7 @@ module common;
 
 import std.stdio;
 
-enum diggerVersion = "2.4";
+enum diggerVersion = "3.0 alpha 1";
 
 /// Send to stderr iff we have a console to write to
 void writeToConsole(string s)
