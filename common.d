8d43718a935533c1458266d65db987e8b7cdb35d
Vladimir Panteleev <git@thecybershadow.net>
2018-05-03 14:58:07 +0000

    common: 3.0 alpha 7

--- common.d
+++ common.d
@@ -2,7 +2,7 @@ module common;
 
 import std.stdio;
 
-enum diggerVersion = "3.0 alpha 5";
+enum diggerVersion = "3.0 alpha 7";
 
 /// Send to stderr iff we have a console to write to
 void writeToConsole(string s)
