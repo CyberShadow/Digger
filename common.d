1199be5140719b7cf68a8ff8b17a0eedadd1f782
Vladimir Panteleev <git@thecybershadow.net>
2020-04-21 02:10:19 +0000

    common: Digger 3.0.1
    This is not the grandiose and long-awaited release; but rather, just a
    hack to get the versioning scheme back on track and compatible with
    Semantic Versioning again. Because of how long Digger stagnated as a
    3.0 alpha, the real next major version, including all of its planned
    breaking changes, will be numbered 4.0.

--- common.d
+++ common.d
@@ -2,7 +2,7 @@ module common;
 
 import std.stdio;
 
-enum diggerVersion = "3.0 alpha 11";
+enum diggerVersion = "3.0.1";
 
 /// Send to stderr iff we have a console to write to
 void writeToConsole(string s)
